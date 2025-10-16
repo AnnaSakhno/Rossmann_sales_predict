22c814ae8ab1c3103b2e998a24ab0203bca3c5a0
AnnaSakhno <sakhno.ann@gmail.com>
2025-10-16 19:43:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import datetime
 
 st.set_page_config(page_title="Rossmann ", page_icon="⭕️", layout="centered")
 
-st.logo(icon_image="Rossmann_Logo.svg")
+st.logo("Rossmann_Logo.svg")
 st.title("Predict sales rate for Rossmann drugstore")
 st.image('rossmann.jpeg')
 
